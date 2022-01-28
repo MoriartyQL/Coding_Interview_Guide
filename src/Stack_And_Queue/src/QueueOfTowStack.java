609ce9f69db6a70a610e8651be5aa8b7443aadbb
MoriartyQL <2508483656@qq.com>
2022-01-28 20:45:04 +0800

    Version 1.2 2022,Jan,28

--- src/Stack_And_Queue/src/QueueOfTowStack.java
+++ src/Stack_And_Queue/src/QueueOfTowStack.java
@@ -11,16 +11,49 @@ public class QueueOfTowStack {
     private Stack<Integer> stackPush;
     private Stack<Integer> stackPop;
 
-    public QueueOfTowStack(){
+    public QueueOfTowStack() {
         this.stackPop = new Stack<Integer>();
         this.stackPush = new Stack<Integer>();
     }
 
+    //添加元素到stackPush中
+    public void add(int pushInt) {
+        stackPush.push(pushInt);
+    }
 
+    /*
+        1、先判断是否都为空，若两个栈都为空，则抛出异常
+        2、如果只有 stackPop 为空，将 stackPush 中的数据导入到 stackPop 中
+        3、返回 stackPop 中栈顶的数字
+    */
+    public int poll() {
+        if (stackPop.empty() && stackPush.empty()) {
+            throw new RuntimeException("stack is empty!");
+        } else if (stackPop.empty()) {
+            while (!stackPush.empty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.pop();
+    }
 
-
-
-
+    /*
+        peek() 方法
+        1、先判断是否都为空，若两个栈都为空，则抛出异常
+        2、如果只有 stackPop 为空，将 stackPush 中的数据导入到 stackPop 中
+        3、返回 stackPop 中栈顶的数字
+    */
+    public int peek() {
+        if (stackPop.empty() && stackPush.empty()) {
+            throw new RuntimeException("Stack is empty!");
+        } else if (stackPop.empty()) {
+            while (!stackPush.empty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        stackPop.peek();
+        return stackPop.pop();
+    }
 
 
 }
